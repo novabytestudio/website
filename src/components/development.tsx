59358c25c96ae5546cecc54c6aad4043b8ea014b
fedetomassini <fedetomassini2@gmail.com>
2024-12-09 11:27:02 -0300

    [Fix import error on build]

--- src/components/development.tsx
+++ src/components/development.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { easeInOut, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { Code, Zap, Users, Rocket } from "lucide-react";
 import { Link } from "react-router-dom";
 
